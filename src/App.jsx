e186ed6e162764813c58d7779b5253ed50d5062d
LeHien6601 <110620160+LeHien6601@users.noreply.github.com>
2024-08-28 04:54:07 +0700

    Update image's url

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import './App.css'
 import {Game} from './Game'
+import logo1 from './assets/ninjadodge.png'
+import logo2 from './assets/fallingshapes.png'
+import logo3 from './assets/hexalink.png'
+import logo4 from './assets/mazexpand.png'
+import logo5 from './assets/shootinggallery.png'
+import logo6 from './assets/playwiththedevil.png'
+import logo7 from './assets/penguinsonice.png'
+import logo8 from './assets/shepherd.png'
 function App() {
   return (
     <>
       <div className="game-list">
-        <Game name="Ninja Dodge" image="/src/assets/ninjadodge.png" winCondition="Hoàn thành màn 2" link="https://haiphan2309.itch.io/ninja-dodge"/>
-        <Game name="Falling Shapes" image="/src/assets/fallingshapes.png" winCondition="Đạt 1000 điểm" link="https://haiphan2309.itch.io/falling-shapes"/>
-        <Game name="Hexalink" image="/src/assets/hexalink.png" winCondition="Hoàn thành 4 màn" link="https://gedusx.itch.io/hexalink"/>
-        <Game name="Mazexpand" image="/src/assets/mazexpand.png" winCondition="Hoàn thành 5 màn" link="https://beastkr.itch.io/mazexpand/"/>
-        <Game name="Shooting Gallery" image="/src/assets/shootinggallery.png" winCondition="Đạt 180 điểm" link="https://gloxiniaaa.github.io/UnityWebGL_Shooting-Gallery/"/>
-        <Game name="Play with the devil" image="/src/assets/playwiththedevil.png" winCondition="Hoàn thành màn 2" link="https://lehien6601.github.io/PlayWithTheDevil/"/>
-        <Game name="Penguins On Ice" image="/src/assets/penguinsonice.png" winCondition="Đạt 6 điểm" link="https://haiphan2309.itch.io/penguins-on-ice"/>
-        <Game name="Shepherd" image="/src/assets/shepherd.png" winCondition="Hoàn thành trò chơi" link="https://gloxiniaaa.github.io/UnityWebGL_Shepherd-For-Eveyone/"/>
+        <Game name="Ninja Dodge" image={logo1} winCondition="Hoàn thành màn 2" link="https://haiphan2309.itch.io/ninja-dodge"/>
+        <Game name="Falling Shapes" image={logo2} winCondition="Đạt 1000 điểm" link="https://haiphan2309.itch.io/falling-shapes"/>
+        <Game name="Hexalink" image={logo3} winCondition="Hoàn thành 4 màn" link="https://gedusx.itch.io/hexalink"/>
+        <Game name="Mazexpand" image={logo4} winCondition="Hoàn thành 5 màn" link="https://beastkr.itch.io/mazexpand/"/>
+        <Game name="Shooting Gallery" image={logo5} winCondition="Đạt 180 điểm" link="https://gloxiniaaa.github.io/UnityWebGL_Shooting-Gallery/"/>
+        <Game name="Play with the devil" image={logo6} winCondition="Hoàn thành màn 2" link="https://lehien6601.github.io/PlayWithTheDevil/"/>
+        <Game name="Penguins On Ice" image={logo7} winCondition="Đạt 6 điểm" link="https://haiphan2309.itch.io/penguins-on-ice"/>
+        <Game name="Shepherd" image={logo8} winCondition="Hoàn thành trò chơi" link="https://gloxiniaaa.github.io/UnityWebGL_Shepherd-For-Eveyone/"/>
       </div>
     </>
   )
--- src/Game.jsx
+++ src/Game.jsx
@@ -2,7 +2,7 @@ import './Game.css'
 export function Game({name, image, winCondition, link}) {
     return <a className="game-container" href={link} target='_blank'>
         <p>{winCondition}</p>
-        <img src={require(image)} alt="Gameplay" width="365" height="150"></img>
+        <img src={image} alt="Gameplay" width="365" height="150"></img>
         <h1>{name}</h1>
     </a>
 }
