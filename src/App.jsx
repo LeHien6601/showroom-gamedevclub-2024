e9f0df29ae6fb676168d8f013ff0abbdec38a8ea
LeHien6601 <110620160+LeHien6601@users.noreply.github.com>
2024-09-03 06:40:10 +0700

    Update responsive

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,11 @@ function App() {
   return (
     <>
       <div className="game-list">
-        <Game name="Ninja Dodge" image={logo1} winCondition="Hoàn thành màn 2" link="https://haiphan2309.itch.io/ninja-dodge"/>
+        <Game name="Ninja Dodge" image={logo1} winCondition="Hoàn thành 2 màn" link="https://haiphan2309.itch.io/ninja-dodge"/>
         <Game name="Falling Shapes" image={logo2} winCondition="Đạt 1000 điểm" link="https://haiphan2309.itch.io/falling-shapes"/>
-        <Game name="Hexalink" image={logo3} winCondition="Hoàn thành 4 màn" link="https://gedusx.itch.io/hexalink"/>
+        <Game name="Hexalink" image={logo3} winCondition="Hoàn thành 4 màn hoặc 30k điểm" link="https://gedusx.itch.io/hexalink"/>
         <Game name="Mazexpand" image={logo4} winCondition="Hoàn thành 5 màn" link="https://beastkr.itch.io/mazexpand/"/>
-        <Game name="Shooting Gallery" image={logo5} winCondition="Đạt 180 điểm" link="https://gloxiniaaa.github.io/UnityWebGL_Shooting-Gallery/"/>
+        <Game name="Shooting Gallery" image={logo5} winCondition="Đạt 3000 điểm" link="https://gloxiniaaa.github.io/UnityWebGL_Shooting-Gallery/"/>
         <Game name="Play with the devil" image={logo6} winCondition="Hoàn thành màn 2" link="https://lehien6601.github.io/PlayWithTheDevil/"/>
         <Game name="Penguins On Ice" image={logo7} winCondition="Đạt 6 điểm" link="https://haiphan2309.itch.io/penguins-on-ice"/>
         <Game name="Shepherd" image={logo8} winCondition="Hoàn thành trò chơi" link="https://gloxiniaaa.github.io/UnityWebGL_Shepherd-For-Eveyone/"/>
