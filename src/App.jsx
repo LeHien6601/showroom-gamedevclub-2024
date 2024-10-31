e4f9b47479509adf7691d24ab6384273956e8009
HienLe2004 <hien.lengoc2004@hcmut.edu.vn>
2024-10-31 20:31:09 +0700

    Repush

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,12 @@ function App() {
         <Game name="Ninja Dodge" image={logo1} winCondition="" link="https://haiphan2309.itch.io/ninja-dodge"/>
         <Game name="Falling Shapes" image={logo2} winCondition="" link="https://haiphan2309.itch.io/falling-shapes"/>
         <Game name="Hexalink" image={logo3} winCondition="" link="https://gedusx.itch.io/hexalink"/>
-        <Game name="Guard Me" image={logo4} winCondition="" link="https://beastkr.itch.io/guard-me/"/>
-        <Game name="Shooting Gallery" image={logo5} winCondition="" link="https://gloxiniaaa.github.io/UnityWebGL_Shooting-Gallery/"/>
-        <Game name="Play with the devil" image={logo6} winCondition="" link="https://lehien6601.github.io/PlayWithTheDevil/"/>
+        <Game name="Guard Me" image={logo4} winCondition="" link="https://beastkr.itch.io/guard-me"/>
+        <Game name="Shooting Gallery" image={logo5} winCondition="" link="https://gloxiniaaa.github.io/UnityWebGL_Shooting-Gallery"/>
+        <Game name="Play with the devil" image={logo6} winCondition="" link="https://lehien6601.github.io/PlayWithTheDevil"/>
         <Game name="Penguins On Ice" image={logo7} winCondition="" link="https://haiphan2309.itch.io/penguins-on-ice"/>
-        <Game name="Shepherd" image={logo8} winCondition="" link="https://gloxiniaaa.github.io/UnityWebGL_Shepherd-For-Eveyone/"/>
-        <Game name="BoomBoom" image={logo9} winCondition="" link="https://gloxiniaaa.github.io/UnityWebGL_BoomBoom/"/>
+        <Game name="Shepherd" image={logo8} winCondition="" link="https://gloxiniaaa.github.io/UnityWebGL_Shepherd-For-Eveyone"/>
+        <Game name="BoomBoom" image={logo9} winCondition="" link="https://gloxiniaaa.github.io/UnityWebGL_BoomBoom"/>
       </div>
     </>
   )
