edb7da32d75f6c9b7f3fe7e1433d09058f90c346
LeHien6601 <110620160+LeHien6601@users.noreply.github.com>
2024-10-31 20:21:45 +0700

    Update new games

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,25 @@ import {Game} from './Game'
 import logo1 from './assets/ninjadodge.png'
 import logo2 from './assets/fallingshapes.png'
 import logo3 from './assets/hexalink.png'
-import logo4 from './assets/mazexpand.png'
+import logo4 from './assets/guard-me.png'
 import logo5 from './assets/shootinggallery.png'
 import logo6 from './assets/playwiththedevil.png'
 import logo7 from './assets/penguinsonice.png'
 import logo8 from './assets/shepherd.png'
+import logo9 from './assets/shepherd.png'
 function App() {
   return (
     <>
       <div className="game-list">
-        <Game name="Ninja Dodge" image={logo1} winCondition="Hoàn thành 2 màn" link="https://haiphan2309.itch.io/ninja-dodge"/>
-        <Game name="Falling Shapes" image={logo2} winCondition="Đạt 1000 điểm" link="https://haiphan2309.itch.io/falling-shapes"/>
-        <Game name="Hexalink" image={logo3} winCondition="Hoàn thành 4 màn hoặc 30k điểm" link="https://gedusx.itch.io/hexalink"/>
-        <Game name="Mazexpand" image={logo4} winCondition="Hoàn thành 5 màn" link="https://beastkr.itch.io/mazexpand/"/>
-        <Game name="Shooting Gallery" image={logo5} winCondition="Đạt 3000 điểm" link="https://gloxiniaaa.github.io/UnityWebGL_Shooting-Gallery/"/>
-        <Game name="Play with the devil" image={logo6} winCondition="Hoàn thành màn 2" link="https://lehien6601.github.io/PlayWithTheDevil/"/>
-        <Game name="Penguins On Ice" image={logo7} winCondition="Đạt 6 điểm" link="https://haiphan2309.itch.io/penguins-on-ice"/>
-        <Game name="Shepherd" image={logo8} winCondition="Hoàn thành trò chơi" link="https://gloxiniaaa.github.io/UnityWebGL_Shepherd-For-Eveyone/"/>
+        <Game name="Ninja Dodge" image={logo1} winCondition="" link="https://haiphan2309.itch.io/ninja-dodge"/>
+        <Game name="Falling Shapes" image={logo2} winCondition="" link="https://haiphan2309.itch.io/falling-shapes"/>
+        <Game name="Hexalink" image={logo3} winCondition="" link="https://gedusx.itch.io/hexalink"/>
+        <Game name="Guard Me" image={logo4} winCondition="" link="https://beastkr.itch.io/guard-me/"/>
+        <Game name="Shooting Gallery" image={logo5} winCondition="" link="https://gloxiniaaa.github.io/UnityWebGL_Shooting-Gallery/"/>
+        <Game name="Play with the devil" image={logo6} winCondition="" link="https://lehien6601.github.io/PlayWithTheDevil/"/>
+        <Game name="Penguins On Ice" image={logo7} winCondition="" link="https://haiphan2309.itch.io/penguins-on-ice"/>
+        <Game name="Shepherd" image={logo8} winCondition="" link="https://gloxiniaaa.github.io/UnityWebGL_Shepherd-For-Eveyone/"/>
+        <Game name="BoomBoom" image={logo9} winCondition="" link="https://gloxiniaaa.github.io/UnityWebGL_BoomBoom/"/>
       </div>
     </>
   )
