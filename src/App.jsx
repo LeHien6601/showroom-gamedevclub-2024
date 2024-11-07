8c220a6c5a10710e5926eee315bded2093cda5c1
HienLe2004 <hien.lengoc2004@hcmut.edu.vn>
2024-11-07 21:16:01 +0700

    Update json

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
 import './App.css'
 import {Game} from './Game'
-import logo1 from './assets/ninjadodge.png'
-import logo2 from './assets/fallingshapes.png'
-import logo3 from './assets/hexalink.png'
-import logo4 from './assets/guard-me.png'
-import logo5 from './assets/shootinggallery.png'
-import logo6 from './assets/playwiththedevil.png'
-import logo7 from './assets/penguinsonice.png'
-import logo8 from './assets/shepherd.png'
-import logo9 from './assets/boomboom.png'
-import logo10 from './assets/fall-blabs.png'
+import logo from '../public/logo.png'
+import {useState, useEffect} from 'react'
 function App() {
+  const [games, setGames] = useState([]);
+  useEffect(() => {
+    fetch('scratch-games.json')
+      .then(response => response.json())
+      .then(data => setGames(data))
+  }, []);
+
+  const [ugames, setUGames] = useState([]);
+  useEffect(() => {
+    fetch('unity-games.json')
+      .then(response => response.json())
+      .then(data => setUGames(data))
+  }, []);
   return (
     <>
+      <div className="top">
+        <a href='https://www.facebook.com/hcmutGDC'><img src={logo} className="logo"></img></a>
+      </div>
       <div className="game-list">
+        {games.map((game) => (
+          <Game name={game.name} image={game.image} link={game.link} key={game.id}/>
+        ))}
+      </div>
+      
+      <div className="game-list">
+        {ugames.map((game) => (
+          <Game name={game.name} image={game.image} link={game.link} key={game.id}/>
+        ))}
+      </div>
+      {/* <div className="game-list">
+        <Game name="Falling Shapes" image={logo2} winCondition="" link="https://haiphan2309.itch.io/falling-shapes"/>
         <Game name="Ninja Dodge" image={logo1} winCondition="" link="https://haiphan2309.itch.io/ninja-dodge"/>
         <Game name="Falling Shapes" image={logo2} winCondition="" link="https://haiphan2309.itch.io/falling-shapes"/>
         <Game name="Hexalink" image={logo3} winCondition="" link="https://gedusx.itch.io/hexalink"/>
@@ -24,7 +44,7 @@ function App() {
         <Game name="Shepherd" image={logo8} winCondition="" link="https://gloxiniaaa.github.io/UnityWebGL_Shepherd-For-Eveyone"/>
         <Game name="BoomBoom" image={logo9} winCondition="" link="https://gloxiniaaa.github.io/UnityWebGL_BoomBoom"/>
         <Game name="Fall Blabs" image={logo10} winCondition="" link="https://haiphan2309.itch.io/fall-blabs"/>
-      </div>
+      </div> */}
     </>
   )
 }
